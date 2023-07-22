006458ddaf15a0de644db0311490f59a6faab2e5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-07-22 13:12:13 +0100

    refactor home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 ">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:gap- md:flex-row">
-        <div className="flex flex-col justify-center h-full px-4 ">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white pt-12">
+    <div name="home" className=" h-screen  w-full bg-gradient-to-b from-black via-black to-gray-800 ">
+      <div className="max-w-screen-lg mobile:mx-auto lg:mx-[10vw] 2xl:mx-auto flex flex-col items-center justify-center h-full p-4 md:gap- md:flex-row">
+        <div className="flex flex-col justify-center items-center h-full px-4 ">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white  ">
             I am a Full Stack Developer
             </h2>
           <p className="text-gray-400 py-2 max-w-md text-justify">
@@ -17,7 +17,7 @@ const Home = () => {
             Ruby, and Ruby on Rails with Postgres, Tailwind and GraphQL.
           </p>
 
-          <div>
+          <div className="py-2">
             <button className="group text-white w-fit px-6 py-2 my-1 flex items-center rounded-md bg-gradient-to-tr from-cyan-500 to-blue-500 cursor-pointer">
               <span className="group-hover:scale-110">Projects </span>
               <span className="px-4 group-hover:rotate-90 duration-300 group-hover:scale-150"><MdKeyboardArrowRight size={25}/></span>
